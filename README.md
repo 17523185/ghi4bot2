f3f9c5d7285bb630b4c2f7980cd725b39bf655c7
17523185 <43628484+17523185@users.noreply.github.com>
2018-12-09 18:20:04 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ghi4bot2
+# LINE@ BOT SimSimi
+
+More Info :
+
+https://www.ilyasafr.net/2017/05/cara-membuat-bot-line-simsimi.html
+
+https://www.youtube.com/watch?v=IUhH4qZfXoQ
---
+++ bot.php
@@ -0,0 +1,95 @@
+<?php
+/*
+copyright @ medantechno.com
+Modified by Ilyasa
+2017
+*/
+require_once('./line_class.php');
+
+$channelAccessToken = 'QOcKeMxFvJmyxOEVef79M37nPWOzr31PDTrS37TZ2Iti0wIi9FsdOB38lT60Rxa5PNeZlgwtgRPU6qiDStLK066bT2lQM4mpePV9Y+ne1NQHr6IViG54cbquRoK5TFm6i7xrNSYDqSm/nXCqDrf+0wdB04t89/1O/w1cDnyilFU='; //Your Channel Access Token
+$channelSecret = '30491a70a691f974e3d7dedb62ae9188';//Your Channel Secret
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+
+$userId 	= $client->parseEvents()[0]['source']['userId'];
+$replyToken = $client->parseEvents()[0]['replyToken'];
+$message 	= $client->parseEvents()[0]['message'];
+$profil = $client->profil($userId);
+$pesan_datang = $message['text'];
+
+if($message['type']=='sticker')
+{	
+	$balas = array(
+							'UserID' => $profil->userId,	
+                                                        'replyToken' => $replyToken,							
+							'messages' => array(
+								array(
+										'type' => 'text',									
+										'text' => 'Terima Kasih Stikernya.'										
+									
+									)
+							)
+						);
+						
+}
+else
+$pesan=str_replace(" ", "%20", $pesan_datang);
+$key = 'c1f9f110-eafb-4ec0-81c1-4583e38fa11a'; //API SimSimi
+$url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
+$json_data = file_get_contents($url);
+$url=json_decode($json_data,1);
+$diterima = $url['response'];
+if($message['type']=='text')
+{
+if($url['result'] == 404)
+	{
+		$balas = array(
+							'UserID' => $profil->userId,	
+                                                        'replyToken' => $replyToken,													
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Mohon Gunakan Bahasa Indonesia Yang Benar :D.'
+									)
+							)
+						);
+				
+	}
+else
+if($url['result'] != 100)
+	{
+		
+		
+		$balas = array(
+							'UserID' => $profil->userId,
+                                                        'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Maaf '.$profil->displayName.' Server Kami Sedang Sibuk Sekarang.'
+									)
+							)
+						);
+				
+	}
+	else{
+		$balas = array(
+							'UserID' => $profil->userId,
+                                                        'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => ''.$diterima.''
+									)
+							)
+						);
+						
+	}
+}
+ 
+$result =  json_encode($balas);
+
+file_put_contents('./reply.json',$result);
+
+
+$client->replyMessage($balas);
